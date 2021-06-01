6a838bbe69bdb822bb878b036f3e65bf6fdce2d2
Oksana Tugusheva <gutuxa@gmail.com>
2021-06-01 20:20:52 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Oksana Tugusheva on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
